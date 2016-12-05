59dbc03d86fcfbcfe7469db4c38904e8e9f6573a
Carl-Philip Hänsch <carli@launix.de>
2016-12-05 08:46:41 +0100

    Code added

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Since PHP 7 removed mysql connector and only left mysqli, but lot of legacy soft
 Just add the following line to a central php file:
 
 `if (!function_exists('mysql_connect')) require_once 'mysqli_wrapper.php';`
+
+If there are functions missing, please add them to mysqli_wrapper.php and ask us to merge your changes.
---
+++ mysqli_wrapper.php
@@ -0,0 +1,41 @@
+<?php
+	$mysqli = null;
+	function mysql_connect($a, $b, $c, $d = null) {
+		global $mysqli;
+		$mysqli = new mysqli($a, $b, $c, $d);
+		return $mysqli;
+	}
+	function mysql_select_db($a, $db = null) {
+		global $mysqli;
+		return ($db?:$mysqli)->select_db($a);
+	}
+	function mysql_query($q, $db = null) {
+		global $mysqli;
+		return ($db?:$mysqli)->query($q);
+	}
+	function mysql_insert_id($db = null) {
+		global $mysqli;
+		return ($db?:$mysqli)->insert_id;
+	}
+	function mysql_fetch_array($r, $rt) {
+		return $r->fetch_array(rt);
+	}
+	function mysql_num_rows($r) {
+		return $r->num_rows();
+	}
+	function mysql_close($db = null) {
+		global $mysqli;
+		return ($db?:$mysqli)->close();
+	}
+	function mysql_real_escape_string($s, $db) {
+		global $mysqli;
+		return ($db?:$mysqli)->real_escape_string($s);
+	}
+	function mysql_fetch_assoc($r) {
+		return $r->fetch_assoc();
+	}
+	function mysql_error($db = null) {
+		global $mysqli;
+		return ($db?:$mysqli)->error();
+	}
+?>
